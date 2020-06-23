b2e116940bb3ee2775abede65bf17ba411c19e34
Max Lamb <maxlambcodes@gmail.com>
2020-06-23 15:02:46 -0700

    finisheded gonna play around with this now

--- test/connect.js
+++ test/connect.js
@@ -1,15 +1,12 @@
 const { MongoClient } = require('mongodb');
-
 let client = null;
 beforeAll(() => {
-  return MongoClient.connect('mongodb://alchemy:s3cret@ds249233.mlab.com:49233/enron', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(c => {
-      client = c;
-    });
+    return MongoClient.connect('mongodb://alchemy:s3cret@ds249233.mlab.com:49233/enron', { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(c => {
+            client = c;
+        });
 });
-
 afterAll(() => {
-  return client.close();
+    return client.close();
 });
-
 module.exports = () => client.db('enron').collection('messages');
